b8de1dcbebe9fe2bf341fe897c2e920ae54eadf6
souznikpasha <souznikpasha@gmail.com>
2020-11-13 11:38:11 +0700

    bug_fix

--- main.cpp
+++ main.cpp
@@ -4,5 +4,20 @@ double my_Pow(double x, int n)
     if (n == 0)
         return 1.0;
     else
+    {   
+        if (x < 0)
+        {
+            if (n % 2 = 0)
+            {
+                x = -x;
+                return x * myPow(x, n - 1);
+            }
+            else
+            {
+                return x * myPow(x, n - 1);
+            }
+        }
         return x * myPow(x, n - 1);
+    }
+
 }
